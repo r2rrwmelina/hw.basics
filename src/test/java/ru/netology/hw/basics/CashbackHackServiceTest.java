866928e3480d50acb2e34bdf24767cff1770d229
MG <gmelina69@gmail.com>
2026-04-26 16:37:03 +0400

    Создание ветки testng в проекте Gradle
    1. Настроить зависимости TestNG в файле build.gradle
    2. Написать тесты для проверки метода remain()

---
+++ src/test/java/ru/netology/hw/basics/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.hw.basics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldShow1BeforeCashback() {
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShow999BeforeCashback() {
+        int amount = 1;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    // Падающий тест
+//    @Test
+//    public void shouldNotShowAmountBeforeCashback() {
+//        int amount = 1000;
+//
+//        int expected = 0;
+//        int actual = service.remain(amount);
+//
+//        Assert.assertEquals(actual, expected);
+//    }
+}
